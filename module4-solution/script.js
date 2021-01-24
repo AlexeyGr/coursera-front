372ba379e5d372b014877fb22b88f37cb2732d56
unknown <gritsaev.alexey@gmail.com>
2021-01-24 17:34:20 +0400

    module 4 assignment solution fixes

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,8 +1,8 @@
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
-  var firstLetter = 'j'
+  var firstLetter = names[i].charAt(0).toLowerCase();
   
-  if (names[i].charAt(0).toLowerCase() === firstLetter) {
+  if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
   } else {
     helloSpeaker.speak(names[i]);
